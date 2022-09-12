c7c0065c2407f13beb663882c8c54a1d1b0e865a
Tyzer1 <Tizara1@protonmail.com>
2022-09-12 15:19:02 +0300

    Init

---
+++ APIObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        private int _allocatedObj;
+        private bool _isDisposed = false;
+        public APIObject(int arg)
+        {
+            MagicAPI.Allocate(arg);
+            _allocatedObj = arg;
+        }
+
+        ~APIObject()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+                return;
+
+            MagicAPI.Free(_allocatedObj);
+            GC.SuppressFinalize(this);
+            _isDisposed = true;
+        }
+    }
+}
